d723bcb853fbaf02c154a2e3a4c3d64e578d2b05
koki-sys <55286703+koki-sys@users.noreply.github.com>
2022-08-03 22:03:16 +0900

    ロード中にロード結果が出てしまうバグを修正

--- src/templates/TaskBox/index.tsx
+++ src/templates/TaskBox/index.tsx
@@ -2,15 +2,63 @@ import {
     Box,
     CardContent,
     Typography,
-    List
+    List,
+    ListItem,
+    ListItemText,
+    Divider,
 } from '@mui/material'
 import LaunchIcon from '@mui/icons-material/Launch'
 import { Link } from 'react-router-dom'
 import { TaskCard } from './style'
-import { TaskDigest } from 'templates/TaskDigest'
+import { ErrorAtom } from 'atoms/Error'
+import { UserAtom } from 'atoms/User'
+import { Api } from 'config/Api'
+import { useEffect, useState } from 'react'
+import { useRecoilState, useRecoilValue } from 'recoil'
+import { BoxLoad } from 'templates/BoxLoad'
+
+type TaskType = {
+    id: number
+    created_at: string | null
+    updated_at: string | null
+    task_id: number
+    user_id: number
+    team_id: number
+    task_name: string
+    flg: number
+    deadline: string
+    team_name: string
+    user_name: string
+    errors: string | null
+}
 
 export const TaskBox = () => {
+    const user = useRecoilValue(UserAtom)
+    const [tasks, setTasks] = useState([])
+    const [isLoad, setIsLoad] = useState(false)
+    const [error, setError] = useRecoilState(ErrorAtom)
 
+    useEffect(() => {
+        Api.get(`/api/task/doing?id=${user.id}`).then((res) => {
+            if (res.data.status === 200) {
+                setTasks(res.data.items)
+                setIsLoad(true)
+            } else if (res.data.length === 0) {
+                console.log(`空白エラー${error}`)
+                setError({
+                    status: res.data.status,
+                    message: '何も入ってないよ',
+                })
+                setIsLoad(true)
+            } else {
+                setError({
+                    status: res.data.status,
+                    message: '通信失敗',
+                })
+                setIsLoad(true)
+            }
+        })
+    }, [])
 
     return (
         <TaskCard>
@@ -39,7 +87,19 @@ export const TaskBox = () => {
                     }}
                 >
                     <List component="nav">
-                        <TaskDigest />
+                        {isLoad == false && <BoxLoad />}
+                        {tasks != undefined &&
+                            tasks.map((task: TaskType) => (
+                                <div key={task.id}>
+                                    <ListItem>
+                                        <ListItemText
+                                            primary={task.task_name}
+                                        />
+                                    </ListItem>
+                                    <Divider />
+                                </div>
+                            ))}
+                        {error != undefined && error.message}
                     </List>
                 </Box>
             </CardContent>
